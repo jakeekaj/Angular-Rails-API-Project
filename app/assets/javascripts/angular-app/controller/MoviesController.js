96fb228400d7d46384761c9c411a85e75f72a266
John Carlo Malto <jakemalto2014@gmail.com>
2016-07-05 11:04:38 -0400

    added sort and order by title/reviews

--- app/assets/javascripts/angular-app/controller/MoviesController.js
+++ app/assets/javascripts/angular-app/controller/MoviesController.js
@@ -2,6 +2,22 @@ app.controller('MoviesController', MoviesController);
 
 function MoviesController(RestfulService) {
   var ctrl = this;
+
+  ctrl.activeButton1 = function(){
+    ctrl.active1 = true;
+    ctrl.active2 = false;
+  };
+
+  ctrl.activeButton2 = function(){
+    ctrl.active2 = true;
+    ctrl.active1 = false;
+  };
+
+  ctrl.sort_data_by = function(name){
+      ctrl.sort_on = name;
+      ctrl.reverse = !(ctrl.reverse);
+    }
+
   ctrl.movies = [];
   ctrl.accessMovies = function(){RestfulService.getMovies().success(function(data){
       ctrl.movies = data;
